428f2f7a3f0e95236c49835cc93f6dcd135883a2
Aboood <abdu3793@stud.kea.dk>
2024-12-04 20:18:53 +0100

    feat: initialize turpo-repo with react + nextjs + shadcn

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@repo/ui/components/ui/button";
+import { Slider } from "@repo/ui/components/ui/slider";
 
 export default function Page() {
   return (
     <main>
       <Button>Click me</Button>
+      <Slider defaultValue={[33]} max={100} step={1} />
     </main>
   );
 }
---
+++ apps/vite/src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@repo/ui/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/ui/card";
+import { Checkbox } from "@repo/ui/components/ui/checkbox";
+import { Table } from "@repo/ui/components/ui/table";
+import { Slider } from "@repo/ui/components/ui/slider";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <Button>Click me</Button>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Slider defaultValue={[33]} max={100} step={1} />
+      sss
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      <Card> assssssssssssssss </Card>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Checkbox />
+      <Table />
+    </>
+  );
+}
+
+export default App;
